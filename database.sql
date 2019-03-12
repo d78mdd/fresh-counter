967a736c4ce473855adad529917464c230b995d6
Dimon Kostadinon <s8dikost@stud.uni-saarland.de>
2019-03-12 20:19:56 +0000

    Upload New File

---
+++ database.sql
@@ -0,0 +1,17 @@
+
+CREATE DATABASE `users` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+`users`
+
+
+
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `number` varchar(5) DEFAULT NULL,
+  `time` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+

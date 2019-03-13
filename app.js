33647797ec53bacb762506d60c0eb1d9396fe416
Dimon Kostadinon <s8dikost@stud.uni-saarland.de>
2019-03-13 21:25:43 +0000

    Replace app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+/*
+queuing system for the fresh counter
+
+
+*/
+
+
+// required packages/modules
 var express = require('express');
 var app = express();
 
@@ -6,6 +14,8 @@ var http = 	require('http').Server(app);
 var io = require('socket.io')(http);
 
 var mysql = require('mysql');
+
+var async = require('async');
 
 
 
@@ -28,47 +38,70 @@ console.log( "server's local address : " + IP );
 app.use(express.static(__dirname));
 
 
+// database connection definition
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'users'
+	host     : 'localhost',
+	user     : 'root',
+	password : 'root',
+	database : 'users'
 });
 
 
 
+//sleep function definition
+function sleep() {
+	return new Promise(resolve => setTimeout(resolve, 3000));  // 3 seconds for "serving" a user
+};
 
-
-
-
-
+//queue definition
+var queue = async.queue(async function (task) {
+	console.log('Serving user ' + '' + '.');
+	await sleep();    	
+	console.log('User ' + '' + ' served.');
+	//callback();
+}, 1);   // 1 is the "concurency" argument for "queue" - "users" to be served one by one and not at the same time
+		// could be increased in a future version to support multiple 'fresh counters' or even multiple shops ( / pay desks ?)
 
 /*
-maybe here the query should read the user with the last ID
-
-maybe if the DB is not created it should create it on its own
+tracking of the queue will only happen while the server is running
+should it ?
 */
 
+
+
+
+
+
+
+
+
+// connect to the database
 connection.connect();
+// if there's an error it would be seen from the SELECT below?
+/*
+maybe if the DB is not created it should create it on its own
+
+*/
 
 
 
+
+//initial selection of the latest user from the database
+//if this fails nothing else should happen - everything else needs this to finish
 connection.query('SELECT * FROM users ORDER BY id DESC LIMIT 1', function(err,rows,fields){
 	if (err)
 		throw err
 
 
-	//console.log('The solution is: ', rows[0].id);
-	
-
 	Count = rows[0].id;  // now we have the last saved value from the DB
 
 	Count++;  // number to be used for the upcoming user
 
-	console.log('and Count comes to be : ', Count);
+	console.log('upcoming user', Count);
 
 
 
+	// definitions of the "routes" for the RPI screen and the user pages
 	app.get('/', function(req, res){
 		res.sendFile('index.html');
 		//res.
@@ -76,13 +109,32 @@ connection.query('SELECT * FROM users ORDER BY id DESC LIMIT 1', function(err,ro
 	app.get('/user', function(req, res){
 		res.sendFile(__dirname+'/user.html');
 	});
+ 
+
+
+ 
+
+
+	// adding to the queue
+	queue.push({name: 'a'}, function (err) {
+	    //console.log('I in');
+	    
+	    //console.log('finished processing user1');
+	});
+	queue.push({name: 'b'}, function (err) {
+	    //console.log('II in');
+
+	   //console.log('finished processing user2');
+	});
 
 
 
 
 
+	// 
 	io.on('connection', function(socket){
 
+		//
 		socket.on('request initial data', function(){
 			console.log('main page initialized');
 			//socket.emit('initial data', Count);
@@ -90,22 +142,33 @@ connection.query('SELECT * FROM users ORDER BY id DESC LIMIT 1', function(err,ro
 		});
 
 
+
+
+		//
 		socket.on('user scans', function(){
-		// server has been "poked" by a visiting user , recording that
+		// server has been "poked" by a visiting user ,
 
 			
-			connection.query('INSERT INTO USERS (`number`,`time`) VALUES ("1", current_timestamp)'  // Date.now
+			// , recording that
+			connection.query('INSERT INTO USERS (`number`,`time`) VALUES ("1", Now())'  
 			, function(err,rows,fields){
 				if (err)
-					throw err
-				console.log('The solution is: ', rows[0]);
+					throw err;
+				console.log('successful insert');
 				}
 			)
 			//we issue the recording into the DB as the "Async callback function"
 			//while that's taking place , we prepare the next code with the following 2 lines
+
+
+			//add the user to the queue
+			queue.push({name: Count});
 		
-			Count++;
-			io.emit('reset code', IP + ':3000/user?id=' + Count);
+
+			//
+			Count++;	// maybe this should go up in the callback - here it would increment even if insert failed
+			io.emit('reset code', IP + ':3000/user?id=' + Count);    // this too
+
 
 			console.log(IP + ':3000/user?id=' + Count + ' is the link for the upcoming user');
 		});
@@ -117,7 +180,7 @@ connection.query('SELECT * FROM users ORDER BY id DESC LIMIT 1', function(err,ro
 
 
 	http.listen(3000, function(){
-	console.log('listening on *:3000');
+		console.log('listening on *:3000');
 	});
 
 
@@ -138,41 +201,9 @@ connection.query('SELECT * FROM users ORDER BY id DESC LIMIT 1', function(err,ro
 
 
 
-
-
-
-
-
 //console.log();  //something apropriate 
 
 //connection.end();   // what if it remains commented?
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 

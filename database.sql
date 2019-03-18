b1e1710dff19cc801a496ce96fb1aba053efa58c
Dimon Kostadinon <s8dikost@stud.uni-saarland.de>
2019-03-18 10:11:44 +0000

    Replace database.sql

--- database.sql
+++ database.sql
@@ -1,16 +1,69 @@
 
+
+
+(
+for RPI :
+
+
+mysql> CREATE USER 'mysqluser'@'localhost' IDENTIFIED BY 'mysqluser';
+
+( flush privileges; )
+
+mysql> grant all privileges on *.* to 'mysqluser'@'localhost';
+
+mysql -umysqluser -pmysqluser
+
+)
+
+
+
+
+
 CREATE DATABASE `users` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
 `users`
 
 
+USE users;
 
 
-CREATE TABLE `users` (
+CREATE TABLE `USERS` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `number` varchar(5) DEFAULT NULL,
   `time` date DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+
+ALTER TABLE `users`.`USERS` 
+CHANGE COLUMN `time` `time` TIMESTAMP NULL DEFAULT NULL ;
+
+
+
+INSERT INTO users (`number`,`time`) VALUES ("1", Now())  ;
+
+
+
+
+
+then edit the app.js file > var connection
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
